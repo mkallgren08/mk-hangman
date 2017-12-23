e80c70727dc47c06b62fdb53b46d4831457e07a7
Michael <mkallgren08@gmail.com>
2017-12-23 10:17:56 -0500

    Building for production

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -3,7 +3,7 @@ import Jumbotron from "../../components/Jumbotron";
 import API from "../../utils/API";
 import APINYT from "../../utils/APINYT"
 import DeleteBtn from "../../components/DeleteBtn";
-import {Button, Glyphicon} from "react-bootstrap";
+import { Button, Glyphicon } from "react-bootstrap";
 import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
 //import { Input, TextArea, FormBtn } from "../../components/Form";
@@ -11,6 +11,9 @@ import { List, ListItem } from "../../components/List";
 class Articles extends Component {
   state = {
     biodiversity: [],
+    characSet: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a',
+      's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x',
+      'c', 'v', 'b', 'n', 'm'],
     wordtoguess: "",
     displayedword: "",
     // This is the array of user entries
@@ -22,7 +25,8 @@ class Articles extends Component {
     displayedwordwidth: "md-12",
     wordtoguesswidth: "md-12",
     showWord: false,
-    displayWord: "display:block;"
+    displayWord: "display:block;",
+    isMobile: false,
 
   };
 
@@ -46,12 +50,17 @@ class Articles extends Component {
     this.parseUserInput(userGuess);
   }
 
-  parseUserInput = (userGuess) => {
+  parseUserInput = (input) => {
+    let userGuess = input;
+    console.log(userGuess.target.value)
+    if (userGuess.target.value) {
+      userGuess = userGuess.target.value
+    }
     let alreadyGuessed = this.state.userGuesses;
     // Selection of valid characters for the game
-    let characSet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-      'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-      'w', 'x', 'y', 'z', " "]
+    // let characSet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+    //   'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+    //   'w', 'x', 'y', 'z', " "]
 
     // This builds the array of userGuesses
 
@@ -63,7 +72,7 @@ class Articles extends Component {
       if (alreadyGuessed.indexOf(userGuess) >= 0) {
         console.log("Repeated entry, entry ignored");
         // Third option: is the character valid (in characSet)?
-      } else if (characSet.indexOf(userGuess) === -1) {
+      } else if (this.state.characSet.indexOf(userGuess) === -1) {
         console.log("Invalid character entry!")
         // Fourth option: everything is valid and the character selected gets pushed
         // to an array logging the choice and checking if it is in the wordtoguess.
@@ -138,22 +147,24 @@ class Articles extends Component {
     if (lives === 0) {
       let losses = this.state.losses;
 
-      this.setState({ 
+      this.setState({
         losses: losses + 1,
         showWord: true,
         displayedwordwidth: "md-6",
         wordtoguesswidth: "md-6",
       });
-      setTimeout(function() {this.resetGame()}.bind(this),3000);
+      setTimeout(function () { this.resetGame() }.bind(this), 3000);
     }
   }
 
   checkForWin = (currentDisplay) => {
     if (currentDisplay.indexOf("_") === -1) {
       console.log("You win!")
-      this.setState({ wins: this.state.wins + 1 })
-
-      this.resetGame()
+      this.setState({
+        wins: this.state.wins + 1,
+        showWord: true,
+      });
+      setTimeout(function () { this.resetGame() }.bind(this), 3000);
     }
   }
 
@@ -165,7 +176,7 @@ class Articles extends Component {
         userGuesses: [],
         userGuessesString: "",
         lives: 9,
-        showWord:false,
+        showWord: false,
         displayedwordwidth: "md-12",
         wordtoguesswidth: "md-12",
       }
@@ -185,10 +196,20 @@ class Articles extends Component {
       "keydown",
       this._handleKeyDown.bind(this)
     )
+
+    // From: https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
+    window.mobileAndTabletcheck = function () {
+      (function (a) {
+        if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw(n|u)|c55\/|capi|ccwa|cdm|cell|chtm|cldc|cmd|co(mp|nd)|craw|da(it|ll|ng)|dbte|dcs|devi|dica|dmob|do(c|p)o|ds(12|d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(|_)|g1 u|g560|gene|gf5|gmo|go(\.w|od)|gr(ad|un)|haie|hcit|hd(m|p|t)|hei|hi(pt|ta)|hp( i|ip)|hsc|ht(c(| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i(20|go|ma)|i230|iac( ||\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|[a-w])|libw|lynx|m1w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|mcr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|([1-8]|c))|phil|pire|pl(ay|uc)|pn2|po(ck|rt|se)|prox|psio|ptg|qaa|qc(07|12|21|32|60|[2-7]|i)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h|oo|p)|sdk\/|se(c(|0|1)|47|mc|nd|ri)|sgh|shar|sie(|m)|sk0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h|v|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl|tdg|tel(i|m)|tim|tmo|to(pl|sh)|ts(70|m|m3|m5)|tx9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas|your|zeto|zte/i.test(a.substr(0, 4))
+        ) this.setState({ isMobile: true });
+      })
+      //(navigator.userAgent || navigator.vendor || window.opera);
+    };
   }
 
   // Initial load of saved articles
   componentDidMount() {
+    console.log(this.state.isMobile)
     this.loadBiodiversity();
     //this.loadWordToGuess();
   };
@@ -275,6 +296,9 @@ class Articles extends Component {
   }
 
   render() {
+    // let letters = this.props.characSet;
+    // let bindBtnClick = this.parseUserInput.bind(this, letter)
+
     return (
       <Container fluid>
         <Row>
@@ -326,6 +350,19 @@ class Articles extends Component {
                 <Row>
                   <Col size="md-6">
                     {/* Hangman Pic Goes Here */}
+                    {/* onClick = {console.log(letter)} */}
+                    {this.state.isMobile ? (
+                      <div>
+                        {this.state.characSet.map((letter, index) => (
+
+                          <Button key={letter} value={letter} onClickCapture={this.parseUserInput} >
+                            {letter}
+                          </Button>
+                        ))}
+                      </div>
+                    ) : (
+                        null
+                      )}
                     <h2> Hangman Pic Goes here </h2>
                   </Col>
                   <Col size="md-6">
@@ -343,7 +380,7 @@ class Articles extends Component {
                               <h4> {this.state.userGuessesString} </h4>
                             ) : (
                                 <Button >
-                                  <Glyphicon glyph = "hand-down" />
+                                  <Glyphicon glyph="hand-down" />
                                   <h3>Use Your Keyboard to Enter Guesses!</h3>
                                 </Button>
                               )}
