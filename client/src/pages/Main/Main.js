14f5a08fb23471ab517deb85f5b2461f420dbc30
Michael <mkallgren08@gmail.com>
2018-01-12 13:58:39 -0500

    Saving changes

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -10,22 +10,43 @@ import { List, ListItem } from "../../components/List";
 
 class Articles extends Component {
   state = {
+    // List of animals that are randomly selected for guessing
     biodiversity: [],
+
+    // Valid character set
     characSet: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a',
       's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x',
       'c', 'v', 'b', 'n', 'm'],
+
+    // Word that is loaded behind the scenes 
     wordtoguess: "",
+
+    // Word that the user sees on the screen
     displayedword: "",
-    // This is the array of user entries
+
+    // Array of user entries
     userGuesses: [],
+
     userGuessesString: "",
+
+    // Number of lives
     lives: 9,
+
+    // Number of wins
     wins: 0,
+
+    // Number of losses
     losses: 0,
+
+    
     displayedwordwidth: "md-12",
     wordtoguesswidth: "md-12",
+
+    // Boolean for showing or hiding the word that the user must guess
     showWord: false,
     displayWord: "display:block;",
+
+    // Boolean for declaring the page setup as a mobile layout or a desktop layout.
     isMobile: false,
 
   };
@@ -53,6 +74,8 @@ class Articles extends Component {
     this.parseUserInput(userGuess);
   }
 
+
+  // Parses user guesses, be they button clicks or keyboard strokes.
   parseUserInput = (input) => {
     let userGuess = input;
     if (userGuess.target.value) {
@@ -60,11 +83,6 @@ class Articles extends Component {
       userGuess = userGuess.target.value
     }
     let alreadyGuessed = this.state.userGuesses;
-    // Selection of valid characters for the game
-    // let characSet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-    //   'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-    //   'w', 'x', 'y', 'z', " "]
-
     // This builds the array of userGuesses
 
     // First option:Check if it was the backspace key pressed
@@ -200,6 +218,9 @@ class Articles extends Component {
       this._handleKeyDown.bind(this)
     )
 
+  // This checks the width of the window and determines whether or not to display a series of 
+  // buttons for user input - it's not fool proof - big tablets may fool it, but it's the most
+  // reliable method I've found to check for whether or not the user is on a mobile device.
     console.log(window.matchMedia("max-width:780px").matches)
     if (window.matchMedia("(max-width: 780px)").matches) {
       this.setState({isMobile:true})
@@ -214,6 +235,8 @@ class Articles extends Component {
   componentDidMount() {
     console.log(this.state.isMobile)
     // From: https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
+    // This is a regex I found that seems to be hit-or-miss on detecting whether or not a user
+    // is on a mobile device or not.
     window.mobileAndTabletcheck = function () {
       (function (a) {
         if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw(n|u)|c55\/|capi|ccwa|cdm|cell|chtm|cldc|cmd|co(mp|nd)|craw|da(it|ll|ng)|dbte|dcs|devi|dica|dmob|do(c|p)o|ds(12|d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(|_)|g1 u|g560|gene|gf5|gmo|go(\.w|od)|gr(ad|un)|haie|hcit|hd(m|p|t)|hei|hi(pt|ta)|hp( i|ip)|hsc|ht(c(| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i(20|go|ma)|i230|iac( ||\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|[a-w])|libw|lynx|m1w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|mcr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|([1-8]|c))|phil|pire|pl(ay|uc)|pn2|po(ck|rt|se)|prox|psio|ptg|qaa|qc(07|12|21|32|60|[2-7]|i)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h|oo|p)|sdk\/|se(c(|0|1)|47|mc|nd|ri)|sgh|shar|sie(|m)|sk0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h|v|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl|tdg|tel(i|m)|tim|tmo|to(pl|sh)|ts(70|m|m3|m5)|tx9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas|your|zeto|zte/i.test(a.substr(0, 4))
@@ -236,20 +259,25 @@ class Articles extends Component {
 
         for (let n = 0; n < diverlist.length; n++) {
           // && namelist.indexOf(diverlist[n]["Common Name"] >= 0)
+
+          // This pulls all the Animals from the biodiversity list in the database and stores 
+          // them in a local array for quicker access.
           if (diverlist[n]["Category"] === "Animal") {
             namelist.push(diverlist[n]["Common Name"].toLowerCase());
           }
         }
 
-        console.log(namelist.length);
+        // console.log(namelist.length);
         this.setState({ biodiversity: namelist });
-        console.log("biodiversity list: " + JSON.stringify(this.state.biodiversity, null, 2));
+        // console.log("biodiversity list: " + JSON.stringify(this.state.biodiversity, null, 2));
         this.loadWordToGuess();
       })
       // console.log(res.data.response.docs);
       .catch(err => console.log(err));
   };
 
+// This is a leftover from my template file but would like to leave it here in case I add 
+// an entry form for feedback in the future
 
   // handle form input
   handleInputChange = event => {
@@ -262,8 +290,8 @@ class Articles extends Component {
     this.readUserInput(event)
   };
 
-  //loads a random entry from a selected category
-  //for testing purposes now, using a static entry
+  //loads a random entry from the Biodiversity list that has been stored as an array 
+  // in the page's state.
   loadWordToGuess = () => {
     //let words = ["flamingo", "ocelot", "pistol shrimp", "cockatiel"]/*["pistol shrimp"]*/
     let words = this.state.biodiversity
@@ -273,7 +301,7 @@ class Articles extends Component {
     //declare an empty array for us to push our "_" symbols to
     let currentlyPicked = []
 
-    // fill the currentlyPicked array with either underscores or spaces
+    // fill the currentlyPicked array with either underscores, spaces, or special characters
     for (let i = 0; i < wordArray.length; i++) {
       switch (wordArray[i] !== " " || wordArray[i] !== "-" || wordArray[i] !== "'") {
         case wordArray[i] === "'":
@@ -291,24 +319,16 @@ class Articles extends Component {
         default:
           currentlyPicked.push("_")
       }
-
-
-      // if (wordArray[i] !== " ") {
-      //   currentlyPicked.push("_")
-      // } else {
-      //   currentlyPicked.push(" ")
-      // }
     }
 
     console.log(currentlyPicked)
     //currentlyPicked = wordArray.fill("_");
     this.setState({ wordtoguess: word });
     this.setState({ displayedword: currentlyPicked });
-  }
+  };
 
+  // This is the function that renders the page in the client's window.
   render() {
-    // let letters = this.props.characSet;
-    // let bindBtnClick = this.parseUserInput.bind(this, letter)
 
     return (
       <Container fluid>
@@ -426,8 +446,6 @@ class Articles extends Component {
 
           </Col>
         </Row >
-        {/* <Row>
-        </Row> */}
       </Container >
     );
   }
