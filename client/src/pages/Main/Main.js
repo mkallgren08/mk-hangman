e6e520101e4cdcdd439d9851f6109d7e11336c1e
mkallgren08 <mkallgren08@gmail.com>
2018-03-16 20:27:35 -0400

    fixed the bug where you couldn't enter button input (on touchscreen devices) and created a simple hangman graphic

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -6,8 +6,27 @@ import DeleteBtn from "../../components/DeleteBtn";
 import { Button, Glyphicon } from "react-bootstrap";
 import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
+import "./Main.css"
 //import { Input, TextArea, FormBtn } from "../../components/Form";
 
+let hangmen = [
+  require('../../images/Hangman-0.jpg'),
+  require('../../images/Hangman-11.jpg'),
+  require('../../images/Hangman-22.jpg'),
+  require('../../images/Hangman-33.jpg'),
+  require('../../images/Hangman-44.jpg'),
+  require('../../images/Hangman-55.jpg'),
+  require('../../images/Hangman-66.jpg'),
+  require('../../images/Hangman-77.jpg'),
+  require('../../images/Hangman-88.jpg'),
+  require('../../images/Hangman-full.jpg')
+]
+
+let keyboard = {
+  firstRow: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+  secondRow: ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+  thirdRow: ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+}
 class Articles extends Component {
   state = {
     // List of animals that are randomly selected for guessing
@@ -38,7 +57,7 @@ class Articles extends Component {
     // Number of losses
     losses: 0,
 
-    
+
     displayedwordwidth: "md-12",
     wordtoguesswidth: "md-12",
 
@@ -48,6 +67,8 @@ class Articles extends Component {
 
     // Boolean for declaring the page setup as a mobile layout or a desktop layout.
     isMobile: false,
+
+    imgURL: hangmen[0]
 
   };
 
@@ -59,29 +80,42 @@ class Articles extends Component {
 
   // creating a keystroke function for keyboards
   _handleKeyDown = (event) => {
-    this.readUserKeyInput(event)
+    let userGuess = event.key.toLowerCase();
+    console.log(userGuess);
+    this.readUserKeyInput(userGuess)
   }
 
   // displaykeyboard
   displayKeyboard = () => {
-    this.setState({isMobile:true})
+    this.setState({ isMobile: true })
   }
 
-  readUserKeyInput = (event) => {
-    let userGuess = event.key.toLowerCase();
-    console.log(userGuess);
+  readUserKeyInput = (userGuess) => {
+
     let BACKSPACE = 8;
-    this.parseUserInput(userGuess);
+    if (this.state.wordtoguess.length > 0) {
+      this.parseUserInput(userGuess);
+    } else {
+      console.log("The word has not loaded quite yet!")
+    }
+
+  }
+
+  readBtnInput = (userGuess) => {
+    console.log(userGuess.target.value);
+    let btnInput = userGuess.target.value;
+    this.parseUserInput(btnInput)
   }
 
 
   // Parses user guesses, be they button clicks or keyboard strokes.
   parseUserInput = (input) => {
     let userGuess = input;
-    if (userGuess.target.value) {
-      console.log(userGuess.target.value)
-      userGuess = userGuess.target.value
-    }
+
+    // if (userGuess.target.value) {
+    //   console.log(userGuess.target.value)
+    //   userGuess = userGuess.target.value
+    // }
     let alreadyGuessed = this.state.userGuesses;
     // This builds the array of userGuesses
 
@@ -132,7 +166,15 @@ class Articles extends Component {
       let livesLeft = this.state.lives;
       livesLeft = livesLeft - 1;
 
+      // This sets the state of the hangman pic
+      let hangmanPercent = 9 - livesLeft;
+      console.log(typeof hangmanPercent)
+      console.log(hangmanPercent)
+
       this.setState({ lives: livesLeft });
+      this.setState({ imgURL: hangmen[hangmanPercent] })
+
+
       this.checkforLoss(livesLeft);
 
     }
@@ -200,6 +242,7 @@ class Articles extends Component {
         showWord: false,
         displayedwordwidth: "md-12",
         wordtoguesswidth: "md-12",
+        imgURL: hangmen[0]
       }
     )
     this.loadWordToGuess()
@@ -218,12 +261,12 @@ class Articles extends Component {
       this._handleKeyDown.bind(this)
     )
 
-  // This checks the width of the window and determines whether or not to display a series of 
-  // buttons for user input - it's not fool proof - big tablets may fool it, but it's the most
-  // reliable method I've found to check for whether or not the user is on a mobile device.
+    // This checks the width of the window and determines whether or not to display a series of 
+    // buttons for user input - it's not fool proof - big tablets may fool it, but it's the most
+    // reliable method I've found to check for whether or not the user is on a mobile device.
     console.log(window.matchMedia("max-width:780px").matches)
     if (window.matchMedia("(max-width: 780px)").matches) {
-      this.setState({isMobile:true})
+      this.setState({ isMobile: true })
       console.log("this is a mobile device")
     } else {
       console.log("this isn't a mobile device")
@@ -242,7 +285,7 @@ class Articles extends Component {
         if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw(n|u)|c55\/|capi|ccwa|cdm|cell|chtm|cldc|cmd|co(mp|nd)|craw|da(it|ll|ng)|dbte|dcs|devi|dica|dmob|do(c|p)o|ds(12|d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(|_)|g1 u|g560|gene|gf5|gmo|go(\.w|od)|gr(ad|un)|haie|hcit|hd(m|p|t)|hei|hi(pt|ta)|hp( i|ip)|hsc|ht(c(| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i(20|go|ma)|i230|iac( ||\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|[a-w])|libw|lynx|m1w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|mcr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|([1-8]|c))|phil|pire|pl(ay|uc)|pn2|po(ck|rt|se)|prox|psio|ptg|qaa|qc(07|12|21|32|60|[2-7]|i)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h|oo|p)|sdk\/|se(c(|0|1)|47|mc|nd|ri)|sgh|shar|sie(|m)|sk0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h|v|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl|tdg|tel(i|m)|tim|tmo|to(pl|sh)|ts(70|m|m3|m5)|tx9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas|your|zeto|zte/i.test(a.substr(0, 4))
         ) this.setState({ isMobile: true });
       })
-      (navigator.userAgent || navigator.vendor || window.opera);
+        (navigator.userAgent || navigator.vendor || window.opera);
     };
     this.loadBiodiversity();
     //this.loadWordToGuess();
@@ -252,32 +295,32 @@ class Articles extends Component {
   loadBiodiversity = () => {
     API.getBiodiversity()
       .then(
-      res => {
-        //console.log("biodiversity list incoming: " + JSON.stringify(res.data, null, 2))
-        let diverlist = res.data;
-        let namelist = [];
+        res => {
+          //console.log("biodiversity list incoming: " + JSON.stringify(res.data, null, 2))
+          let diverlist = res.data;
+          let namelist = [];
 
-        for (let n = 0; n < diverlist.length; n++) {
-          // && namelist.indexOf(diverlist[n]["Common Name"] >= 0)
+          for (let n = 0; n < diverlist.length; n++) {
+            // && namelist.indexOf(diverlist[n]["Common Name"] >= 0)
 
-          // This pulls all the Animals from the biodiversity list in the database and stores 
-          // them in a local array for quicker access.
-          if (diverlist[n]["Category"] === "Animal") {
-            namelist.push(diverlist[n]["Common Name"].toLowerCase());
+            // This pulls all the Animals from the biodiversity list in the database and stores 
+            // them in a local array for quicker access.
+            if (diverlist[n]["Category"] === "Animal") {
+              namelist.push(diverlist[n]["Common Name"].toLowerCase());
+            }
           }
-        }
 
-        // console.log(namelist.length);
-        this.setState({ biodiversity: namelist });
-        // console.log("biodiversity list: " + JSON.stringify(this.state.biodiversity, null, 2));
-        this.loadWordToGuess();
-      })
+          // console.log(namelist.length);
+          this.setState({ biodiversity: namelist });
+          // console.log("biodiversity list: " + JSON.stringify(this.state.biodiversity, null, 2));
+          this.loadWordToGuess();
+        })
       // console.log(res.data.response.docs);
       .catch(err => console.log(err));
   };
 
-// This is a leftover from my template file but would like to leave it here in case I add 
-// an entry form for feedback in the future
+  // This is a leftover from my template file but would like to leave it here in case I add 
+  // an entry form for feedback in the future
 
   // handle form input
   handleInputChange = event => {
@@ -344,11 +387,7 @@ class Articles extends Component {
                 <h3 className="panel-title"><strong><i className="fa fa-table"></i>  Word to Guess</strong></h3>
               </div>
               <div className="panel-body" id="well-section">
-                <List>
-                  <ListItem>
-                    <h3>{this.state.displayedword}</h3>
-                  </ListItem>
-                </List>
+                <h3>{this.state.displayedword}</h3>
               </div>
             </div>
           </Col>
@@ -383,18 +422,41 @@ class Articles extends Component {
                     {/* Hangman Pic Goes Here */}
                     {/* onClick = {console.log(letter)} */}
                     {this.state.isMobile ? (
-                      <div>
-                        {this.state.characSet.map((letter, index) => (
+                      <div id="keyboardWrapper">
+                        <div id="keyboard">
+                          <div className="keyboardRow">
+                            {keyboard.firstRow.map((letter, index) => (
 
-                          <Button key={letter} value={letter} onClickCapture={this.parseUserInput} >
-                            {letter}
-                          </Button>
-                        ))}
+                              <Button key={letter} value={letter} onClickCapture={this.readBtnInput} className="keyboardBtn">
+                                {letter}
+                              </Button>
+                            ))}
+                          </div>
+                          <div className="keyboardRow">
+                            {keyboard.secondRow.map((letter, index) => (
+
+                              <Button key={letter} value={letter} onClickCapture={this.readBtnInput} >
+                                {letter}
+                              </Button>
+                            ))}
+                          </div>
+                          <div className="keyboardRow">
+                            {keyboard.thirdRow.map((letter, index) => (
+
+                              <Button key={letter} value={letter} onClickCapture={this.readBtnInput} >
+                                {letter}
+                              </Button>
+                            ))}
+                          </div>
+                        </div>
                       </div>
                     ) : (
                         null
                       )}
                     <h2> Hangman Pic Goes here </h2>
+                    <div>
+                      <img src={this.state.imgURL} alt="Hangman image" id="hangman-pic" />
+                    </div>
                   </Col>
                   <Col size="md-6">
                     <Row>
@@ -410,7 +472,7 @@ class Articles extends Component {
                             {this.state.userGuesses.length ? (
                               <h4> {this.state.userGuessesString} </h4>
                             ) : (
-                                <Button onClickCapture = {this.displayKeyboard}>
+                                <Button onClickCapture={this.displayKeyboard}>
                                   <Glyphicon glyph="hand-down" />
                                   <h3>Use Your Keyboard to Enter Guesses!</h3>
                                 </Button>
